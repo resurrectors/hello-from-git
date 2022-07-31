24c9b1047c79fd194694986e6cfc5905edd1da1c
resurrectors <shubhampatil26199@gmail.com>
2022-07-31 20:15:30 +0530

    glsdkf

---
+++ Ascii.java
@@ -0,0 +1,6 @@
+public class Ascii{
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println((int)c);    
+    }
+}
---
+++ evenodd.java
@@ -0,0 +1,9 @@
+public class evenodd {
+    public static void main(String[] args) {
+        int no = 98;
+        if(no%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
+}
